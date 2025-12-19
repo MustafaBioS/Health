078d2ea3af24db624cd35e1a25bde43b898ca134
MustafaBioS <mustaafahany@gmail.com>
2025-12-20 00:38:28 +0200

    ADD - Onload animation and login page style

--- Health/myapp/static/js/script.js
+++ Health/myapp/static/js/script.js
@@ -1,5 +1,6 @@
 const sign = document.querySelector("#btn");
 const login = document.querySelector("#btn2");
+const anim = document.querySelectorAll('.anim');
 
 sign.addEventListener('click', ()=> {
     window.location.href = sign.dataset.createUrl;
@@ -7,4 +8,10 @@ sign.addEventListener('click', ()=> {
 
 login.addEventListener('click', ()=> {
     window.location.href = login.dataset.loginUrl;
+})
+
+window.addEventListener('load', ()=> {
+    anim.forEach(animation => {
+        animation.classList.add('animate');
+    });
 })
